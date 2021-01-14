d6816663bb4f2b31a2cb483de697e3abc2288e30
Syahra-Z <syahrazulya9@gmail.com>
2021-01-14 13:01:57 +0700

    sukses

---
+++ src/praktikum2/Praktikum2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum2;
+
+/**
+ *
+ * @author MOKLET-01
+ */
+public class Praktikum2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //deklarasi variabel
+        int a=4;
+        //perulangan
+        for (int b=1; b<=a; b++){
+            for (int c=1; c<=b; c++){
+                //output
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+    }
+    
+}
